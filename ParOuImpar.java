98cd471e0c658eb0178e18c7dde8999f13543ce6
pedrocavaliere <pedrokuchpil@gmail.com>
2016-07-24 11:32:53 -0300

    Substitui array por lista

--- ParOuImpar.java
+++ ParOuImpar.java
@@ -3,9 +3,11 @@ import java.util.*;
 public class ParOuImpar {
 	public static void main(String[] args) {
 		Scanner input = new Scanner (System.in);
+		
+		List<String> ganhadores = new ArrayList<String>();
+		
 		int n = input.nextInt();
 		while (n!=0){
-			String [] ganhadores = new String[n];
 			String jogadorPar = input.nextLine();
 			String jogadorImpar = input.nextLine();
 			
@@ -14,14 +16,14 @@ public class ParOuImpar {
 				int jogada2 = input.nextInt();
 				
 				if ((jogada1+jogada2)%2==0){
-					ganhadores [i] = jogadorPar;
+					ganhadores.add(jogadorPar);
 				}
 				else{
-					ganhadores[i] = jogadorImpar;
+					ganhadores.add(jogadorImpar);
 				}
-			}
-			
+			}			
 			n = input.nextInt();
 		}
+		
 	}
 }
