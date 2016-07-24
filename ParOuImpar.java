cfed3eeb49f364ddbb7c11639028a2a2a50cc1f6
pedrocavaliere <pedrokuchpil@gmail.com>
2016-07-23 23:41:56 -0300

    Cria classe e numero de testes

---
+++ ParOuImpar.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ParOuImpar {
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		int n = input.nextInt();
+		while (n!=0){
+			
+		}
+	}
+}
