efaa47fedc3f5d464ab094d1dc2d0bd46b16c5e9
pedrocavaliere <pedrokuchpil@gmail.com>
2016-07-23 23:52:06 -0300

    Adiciona jogadores

--- ParOuImpar.java
+++ ParOuImpar.java
@@ -5,7 +5,16 @@ public class ParOuImpar {
 		Scanner input = new Scanner (System.in);
 		int n = input.nextInt();
 		while (n!=0){
+			String [] ganhadores = new String[n];
+			String jogadorPar = input.nextLine();
+			String jogadorImpar = input.nextLine();
 			
+			for (int i=0; i<n; i++){
+				int jogada1 = input.nextInt();
+				int jogada2 = input.nextInt();
+			}
+			
+			n = input.nextInt();
 		}
 	}
 }
